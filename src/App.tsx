0be28c935e0099f87ba2a586711506b2fedcdd66
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-12 23:04:37 +0530

    feat: autocomplete component layout created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Autocomplete from "./react/autocomplete/AutoComplete";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Autocomplete />
+    </div>
+  );
 }
 
 export default App;
