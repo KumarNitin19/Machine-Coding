3e1782640e8067585bac2799f251c31438ad87f3
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-13 23:01:18 +0530

    fix: styles added to autocomplete component

--- src/react/autocomplete/AutoComplete.tsx
+++ src/react/autocomplete/AutoComplete.tsx
@@ -20,16 +20,28 @@ const Autocomplete: React.FC = () => {
     fetchUsers();
   }, [inputVal]);
 
+  useEffect(() => {
+    addStyle();
+  }, []);
+
+  const chip = ["nitin", "hello"];
+
   return (
     <div className="react-autocomplete">
-      <div className="autocomplete-chip"></div>
-      <input
-        type="text"
-        placeholder="Search Something!"
-        onChange={(e) => setInputVal(e.target.value)}
-      />
+      <div className="autocomplete-input">
+        <div className="autocomplete-chip">
+          {chip?.length
+            ? chip?.map((item) => <div className="chip">{item}</div>)
+            : ""}
+        </div>
+        <input
+          type="text"
+          placeholder="Search Something!"
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+      </div>
       <div className="autocomplete-listBox">
-        <ul>
+        <ul className="autocomplete-list">
           {suggestions?.length
             ? suggestions?.map((item) => (
                 <li key={item?.email}>
@@ -37,6 +49,7 @@ const Autocomplete: React.FC = () => {
                 </li>
               ))
             : ""}
+          w
         </ul>
       </div>
     </div>
@@ -44,3 +57,66 @@ const Autocomplete: React.FC = () => {
 };
 
 export default Autocomplete;
+
+const addStyle = () => {
+  const Autocomplete = ` 
+      .react-autocomplete{
+        width:50%;
+      }
+      .autocomplete-input{
+        position:relative;
+        display:flex;
+        flex-wrap:wrap;
+      }
+      .autocomplete-chip{
+        position: absolute;
+        top: 4px;
+        left: 8px;
+        display: flex;
+        gap: 4px;
+      }
+      .autocomplete-chip .chip{
+        background: whitesmoke;
+        border-radius:16px;
+        padding:4px;
+      }
+      .react-autocomplete input{
+          font-size:16px;
+          border-radius:6px;
+          border:0.5px solid grey;   
+          width:100%;
+          padding: 4px 8px;
+          box-sizing: border-box;
+      }
+      .react-autocomplete input:focus{
+        outline:none;
+      }
+      .autocomplete-listBox{
+        width:100%;
+        margin-top:4px;
+        border-radius:4px;
+        box-shadow:0 0 2px rgba(0,0,0,0.1);
+        padding: 4px;
+      }
+      .autocomplete-list{
+        list-style: none;
+        padding: 0 8px;
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        font-size: 16px;
+      }
+      .autocomplete-list li{
+        padding:4px;
+      }
+      .autocomplete-list li:hover{
+        background:rgba(0,0,0,0.1);
+        border-radius:6px;
+        cursor:pointer;
+      }
+      `;
+  const styled = document.createElement("style");
+
+  styled.innerHTML = Autocomplete;
+  document.head.appendChild(styled);
+};
