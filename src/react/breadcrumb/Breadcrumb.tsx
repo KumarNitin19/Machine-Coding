09d21ba005ad05ba0fb36afba954c16bcaec9677
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-16 23:00:05 +0530

    fix: some styling added to breadcrumb

--- src/react/breadcrumb/Breadcrumb.tsx
+++ src/react/breadcrumb/Breadcrumb.tsx
@@ -1,11 +1,17 @@
+import * as React from "react";
+import { useEffect } from "react";
 import { BREADCRUMB_LIST } from "./Breadcrumb.constant";
 
 const Breadcrumb = () => {
+  useEffect(() => {
+    addStyle();
+  }, []);
   return (
     <div className="react-breadcrumb">
       {BREADCRUMB_LIST?.map((item, index) => (
-        <div key={item?.label} className="breadcrumb">
-          {`${item?.label} ${index === BREADCRUMB_LIST?.length - 1 ? "" : ">"}`}
+        <div key={item?.label} className="react-breadcrumb-label">
+          <div>{item?.label}</div>
+          <div>{index === BREADCRUMB_LIST?.length - 1 ? "" : ">"}</div>
         </div>
       ))}
     </div>
@@ -13,3 +19,24 @@ const Breadcrumb = () => {
 };
 
 export default Breadcrumb;
+
+const addStyle = () => {
+  const Breadcrumb = ` 
+    .react-breadcrumb, .react-breadcrumb-label{
+        display:flex;
+        gap:8px;
+    }
+    .react-breadcrumb-label{
+        cursor:pointer;
+        font-size:14px;
+        font-weight:500;
+    }
+    .active{
+      color:blue;
+    }
+      `;
+  const styled = document.createElement("style");
+
+  styled.innerHTML = Breadcrumb;
+  document.head.appendChild(styled);
+};
