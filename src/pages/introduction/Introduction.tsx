08bba7e6e46c23b7e0c28ef561fd31cfb7582a03
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-17 00:53:19 +0530

    update: website enhancements

---
+++ src/pages/introduction/Introduction.tsx
@@ -0,0 +1,5 @@
+const Introduction: React.FC = () => {
+  return <div className="introduction"></div>;
+};
+
+export default Introduction;
--- src/react/timeline/Timeline.tsx
+++ src/react/timeline/Timeline.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { addStyle } from "../../utils/addStyle";
 import { TIMELINE } from "./Timeline.constant";
 
 const Timeline: React.FC = () => {
   const [activeTab, setActiveTab] = useState<number>(0);
+
+  const styles = `
+      .react-timeline{
+        display:flex;
+        align-items:center;
+        gap:8px;
+      }
+      .react-timeline-tab{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        height:32px;
+        width:32px;
+        border-radius:50%;
+        border:1px solid rgba(0,0,0,0.1);
+        background:whitesmoke;
+      }
+      .react-timeline-bar{
+        height: 2px;
+        width:50px;
+        background:whitesmoke;
+      }
+      .react-timeline .active{
+        background:lightgreen;
+        color:white;
+        font-weight:500;
+      }
+      .react-timeline-next button{
+        border:none;
+        padding:8px 12px;
+        cursor:pointer;
+        margin-left:12px;
+      }
+  `;
+
   useEffect(() => {
-    addStyle();
+    addStyle(styles);
   }, []);
 
   const handleNext = () => {
@@ -46,43 +82,3 @@ const Timeline: React.FC = () => {
 };
 
 export default Timeline;
-
-const addStyle = () => {
-  const Timeline = `
-  .react-timeline{
-    display:flex;
-    align-items:center;
-    gap:8px;
-  }
-  .react-timeline-tab{
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    height:32px;
-    width:32px;
-    border-radius:50%;
-    border:1px solid rgba(0,0,0,0.1);
-    background:whitesmoke;
-  }
-  .react-timeline-bar{
-    height: 2px;
-    width:50px;
-    background:whitesmoke;
-  }
-  .active{
-    background:lightgreen;
-    color:white;
-    font-weight:500;
-  }
-  .react-timeline-next button{
-    border:none;
-    padding:8px 12px;
-    cursor:pointer;
-    margin-left:12px;
-  }
-  `;
-  const styled = document.createElement("style");
-
-  styled.innerHTML = Timeline;
-  document.head.appendChild(styled);
-};
