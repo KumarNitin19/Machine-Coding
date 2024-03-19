aa259ed1b5826af67335dd68e2be41c5eef8b30f
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-19 23:56:44 +0530

    fix: html layout created for stop watch component

--- src/react/stopWatch/StopWatch.tsx
+++ src/react/stopWatch/StopWatch.tsx
@@ -2,7 +2,27 @@ import React from "react";
 const StopWatch: React.FC = () => {
   return (
     <div className="stop-watch">
-      <div>StopWatch</div>
+      <div className="stop-watch-label">Stop Watch</div>
+      <div className="stop-watch-timer">
+        <div className="stop-watch-timer-hours">
+          <div className="stop-watch-timer-key">Hours</div>
+          <div className="stop-watch-timer-value">00</div>
+        </div>
+        <div>:</div>
+        <div className="stop-watch-timer-minutes">
+          <div className="stop-watch-timer-key">Minutes</div>
+          <div className="stop-watch-timer-value">00</div>
+        </div>
+        <div>:</div>
+        <div className="stop-watch-timer-seconds">
+          <div className="stop-watch-timer-key">Seconds</div>
+          <div className="stop-watch-timer-value">00</div>
+        </div>
+      </div>
+      <div className="stop-watch-actions">
+        <button className="stop-watch-timer-reset">Reset</button>
+        <button className="stop-watch-timer-start">Start</button>
+      </div>
     </div>
   );
 };
