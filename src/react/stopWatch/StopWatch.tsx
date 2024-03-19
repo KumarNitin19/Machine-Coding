61a8da4fa58d435e8090f36ed30a0385e7120f5c
Nitinkumar19 <nitinkumardeshmukh19@gmail.com>
2024-03-19 23:50:51 +0530

    feat: stop watch component created

---
+++ src/react/stopWatch/StopWatch.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const StopWatch: React.FC = () => {
+  return (
+    <div className="stop-watch">
+      <div>StopWatch</div>
+    </div>
+  );
+};
+
+export default StopWatch;
