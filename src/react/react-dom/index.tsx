16a0defbab2178d1703c9b5fb577aef9717aa593
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2025-03-02 20:23:51 +0530

    feat: react dom generator component created

--- src/react/react-dom/index.tsx
+++ src/react/react-dom/index.tsx
@@ -28,27 +28,42 @@ const reactObj = {
   ],
 };
 
-const DOMGenerator = (reactObj, parentId) => {
+const DOMGenerator = (reactObj) => {
   let { props, type, children } = reactObj;
+  let id = null;
+  let domChild = null;
+
+  if (typeof props === "object" && props.id) {
+    id = props.id;
+  }
+
   const node = document.createElement(type);
 
-  if (props && props.id) {
-    node.id = props.id;
+  if (typeof id === "string") {
+    node.id = id;
+    node.class = "hell0";
   }
 
-  if (Array.isArray(children)) {
-    node.append(children?.map((child) => DOMGenerator(child, props?.id)));
-  } else {
+  if (children && Array.isArray(children)) {
+    domChild = children;
+  } else if (typeof children === "string") {
     node.innerText = children;
   }
-  document.getElementById(parentId)?.append(node);
 
-  //   return node;
+  if (Array.isArray(domChild)) {
+    const childElements = document?.createDocumentFragment();
+    for (const child of domChild) {
+      childElements.append(DOMGenerator(child));
+    }
+    node.append(childElements);
+  }
+  return node;
 };
 
 const ReactDom = () => {
   useEffect(() => {
-    DOMGenerator(reactObj, "dynamic-dom");
+    const root = DOMGenerator(reactObj);
+    document.getElementById("dynamic-dom")?.append(root);
   }, [reactObj]);
 
   return <div id="dynamic-dom"></div>;
